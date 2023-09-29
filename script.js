b3c9b15f714ec86ce262b63ed23915267ee33e35
Navadeep Komarraju <90325434+Navadeep2000@users.noreply.github.com>
2023-09-29 18:31:48 +0530

    Beginner practice for HTML, CSS, JS

---
+++ script.js
@@ -0,0 +1,23 @@
+function ingredientsHover() {
+  document.getElementById("coffee").style.fontSize = "300%";
+}
+
+function ingredientsNormal() {
+  document.getElementById("coffee").style.fontSize = "100%";
+}
+
+function preparationHover() {
+  document.getElementById("book").style.fontSize = "300%";
+}
+
+function preparationNormal() {
+  document.getElementById("book").style.fontSize = "100%";
+}
+
+function CopyMoveLeft() {
+  document.getElementById("footer").style.textAlign = "right";
+}
+
+function CopyMoveRight() {
+  document.getElementById("footer").style.textAlign = "start";
+}
